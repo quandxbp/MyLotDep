8561e080019ab8aff8c81434605eed2c3b613f0f
Quân Bùi <quan.bui@novobi.com>
2019-12-11 10:03:19 +0700

    add credential

---
+++ common/credentials.py
@@ -0,0 +1,20 @@
+DATABASE_CRE = {
+    'DB_NAME': 'recommender',
+    'USER': 'root',
+    'PASSWORD': 'Hungvuong12',
+    'HOST': '127.0.0.1',
+    'PORT': '3306'
+}
+
+MONGO_CRE = {
+    'DB_NAME': 'lotdep-timeseries',
+    'USER': 'quandxbp',
+    'PASSWORD': 'Hungvuong12',
+    'PORT': '33248',
+    'AUTH_SOURCE': 'lotdep-timeseries',
+}
+
+ACCESSTRADE_CRE = {
+    'ACCESS_KEY': 'hIAO1n_PcfcRwUIKcpMqnJneuGQ-YrtC',
+    'SECRET_KEY': 'obzlw+lj@f5)orhvn95-(f@t2pg_srm0'
+}
